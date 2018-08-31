4560adfbc83fc9509766a6ac084a6f09c0654519
Victor Capone <capone.victor@yahoo.com.br>
2018-08-31 20:41:14 -0300

    Adicionando arquivos do pycharm ao ignore, reaftorando código

--- codigo/generateToyExplanation.py
+++ codigo/generateToyExplanation.py
@@ -2,15 +2,13 @@ from mime import *
 import pmlb
 import pandas as pd
 from sklearn.svm import SVC
-from sklearn.metrics import mutual_info_score
 import matplotlib.pyplot as plt
 
-x,y = pmlb.fetch_data('iris', return_X_y=True)
+x, y = pmlb.fetch_data('iris', return_X_y=True)
 data = pd.DataFrame(x)
-mime = Mime(data,y)
+mime = Mime(data, y)
 blackBox = SVC()
 blackBox.fit(x[:100], y[:100])
-#print([mutual_info_score(data.T.values[i], y) for i in range(4)])
 all_explanations = []
 for i in range(100):
     explanation, pred = mime.explain(x[101], blackBox.predict)
--- codigo/mime.py
+++ codigo/mime.py
@@ -2,32 +2,33 @@
 import math
 from numpy import digitize, linspace
 import random
-import sklearn.metrics as skm
 import pandas as pd
+
 
 def prob(e, space):
     p = float(space.count(e))/len(space)
     return p
 
-def weigthed_mutual_information(x, y, weights):
+
+def weighted_mutual_information(x, y, weights):
     x = list(x)
     y = list(y)
     if len(weights) != len(x[0]):
         raise ValueError("There must be as many features as there are weights sets")
-    xT = [list(column) for column in zip(*x)]
-    uY = set(y)
-    mi = [0] * len(xT)
-    for index, column in enumerate(xT):
-        uX = set(column)
-        joint = list(zip(y,column))
-        for vY in uY:
+    xt = [list(column) for column in zip(*x)]
+    uy = set(y)
+    mi = [0] * len(xt)
+    for index, column in enumerate(xt):
+        joint = list(zip(y, column))
+        for vY in uy:
             for i, vX in enumerate(column):
                 w = weights[index][vX] 
-                p = prob((vY,vX),joint)
-                prob_ratio = float(p)/(prob(vX,column)*prob(vY,y))
+                p = prob((vY, vX), joint)
+                prob_ratio = float(p)/(prob(vX, column)*prob(vY, y))
                 if p > 0:
-                    mi[index] +=  w * p * math.log(prob_ratio)
+                    mi[index] += w * p * math.log(prob_ratio)
     return mi
+
 
 # Used to encapsulate all data and metadata for message passing between layers in mime
 class DataWrapper(object):
@@ -36,6 +37,7 @@ class DataWrapper(object):
         self.data = data
         self.categorical = categorical
         self.target = target
+
 
 # Before explaining our instances we may need to do
 # some pre-calculations for some reason, this is
@@ -47,30 +49,33 @@ class MimePreprocessor(object):
         self.data = data
         self.args = kwargs
         self.computed = kwargs
+        self.compute_params(self.data)
+
+    def compute_params(self, data):
+        self.computed["optimalBins"] = int(math.log(data.shape[0], 2) + 1)
+        self.computed["sampleSize"] = int(0.1 * data.shape[0])  # 10% of the data size
 
     def preprocess(self, data=None):
-        if data==None:
-            workingData = self.data
-        else:
-            workingData = data
+        if data is not None:
+            working_data = data
+            self.compute_params(working_data)
         # For MIME we need to precompute the optimal number of bins for each non-categorical feature
         # We do this using Sturge's Formula and the Sample Size
         # Then we change our data to the binned version
-        self.computed["optimalBins"] = int(math.log(workingData.data.shape[0], 2) + 1)
-        self.computed["sampleSize"] = int(0.1 * workingData.data.shape[0]) # 10% of the data size
         self.computed["bins"] = []
-        dataCopy = workingData.data.copy()
-        tmp = workingData.data.T.values
-        for i in range(workingData.data.shape[1]):
-            if i in workingData.categorical:
+        data_copy = working_data.data.copy()
+        tmp = working_data.data.T.values
+        for i in range(working_data.data.shape[1]):
+            if i in working_data.categorical:
                 continue
-            #Digitizes the values on the given columns
+            # Digitizes the values on the given columns
             bins = linspace(min(tmp[i]), max(tmp[i]), self.computed["optimalBins"])
             self.computed["bins"].append(bins)
             binned = digitize(tmp[i], bins)
-            dataCopy[dataCopy.columns[i]] = binned
-        newData = DataWrapper(dataCopy, workingData.target, workingData.categorical)
-        return newData
+            data_copy[data_copy.columns[i]] = binned
+        new_data = DataWrapper(data_copy, working_data.target, working_data.categorical)
+        return new_data
+
 
 class Sampler(object):
 
@@ -86,29 +91,30 @@ class Sampler(object):
             neighborhood.append(sample)
         return neighborhood
 
+
 # At last, the explainer takes an instance and pre-computed data
 # and generates an explanation for it
 class MimeExplainer(object):
 
     def __init__(self, sampler=Sampler(), **kwargs):
-       self.args = kwargs
-       self.sampler = sampler
+        self.args = kwargs
+        self.sampler = sampler
 
     # You can use kwargs to pass parameters precomputed by the preprocessor
     # MimeExplainer expects a "sampleSize" parameter
     def explain(self, instance, data, predictor, preprocessor):
-        # Preprocess data
-        processedData = preprocessor.preprocess()
         # Generate neighborhood samples
-        neighborhood = pd.DataFrame(self.sampler.sample_neighborhood(instance, data, preprocessor.computed['sampleSize']));
+        neighborhood = pd.DataFrame(self.sampler.sample_neighborhood(instance, data,
+                                                                     preprocessor.computed['sampleSize']))
         # Label the samples
-        neighborhoodLabels = predictor(neighborhood)
+        neighborhood_labels = predictor(neighborhood)
         # Weight the samples
         bins_weights = self.weight_bins(instance, preprocessor.computed["bins"])
-        neighborhoodData = DataWrapper(neighborhood, neighborhoodLabels, data.categorical)
-        neighborhoodData = preprocessor.preprocess(neighborhoodData)
+        neighborhood_data = DataWrapper(neighborhood, neighborhood_labels, data.categorical)
+        neighborhood_data = preprocessor.preprocess(neighborhood_data)
         # For each feature, calculates its mutual information with the labels
-        return weigthed_mutual_information(neighborhoodData.data.values, neighborhoodLabels, bins_weights), predictor([instance])
+        return weighted_mutual_information(neighborhood_data.data.values,
+                                           neighborhood_labels, bins_weights), predictor([instance])
 
     def weight_bins(self, instance, bins):
         w = []
@@ -117,12 +123,13 @@ class MimeExplainer(object):
             bins_w = []
             bins_width = bins[0][1]-bins[0][0]
             for b in range(len(bins[0])):
-                #Falta, gerar a última bin
-                bin_midpoint = bins[f][b]-(bins_width)/2
+                # Falta, gerar a última bin
+                bin_midpoint = bins[f][b]-bins_width/2
                 dsqr = (instance[f] - bin_midpoint)**2
                 bins_w.append(math.exp(-dsqr/width))
             w.append(bins_w)
         return w
+
 
 # MIME is a Mutual Information Model-Agnostic Explanator for machine learning
 # black boxes, it uses a similar aproach to that of LIME, probing the decision
@@ -137,10 +144,10 @@ class Mime(object):
     # parameters   : the parameters (if any) you want to pass to your preprocessor or explainer, should be a dict
     def __init__(self, dataframe, target, categorical=[],
                  preprocessor=MimePreprocessor, explainer=MimeExplainer,
-                 preprocessorParameters={}, explainerParameters={}):
+                 preprocessor_parameters={}, explainer_parameters={}):
         self.data = DataWrapper(dataframe, target, categorical)
-        self.preprocessor = preprocessor(self.data, **preprocessorParameters)
-        self.explainer = explainer(**explainerParameters)
+        self.preprocessor = preprocessor(self.data, **preprocessor_parameters)
+        self.explainer = explainer(**explainer_parameters)
 
     # Parameters:
     # instance  : the instance being explained
